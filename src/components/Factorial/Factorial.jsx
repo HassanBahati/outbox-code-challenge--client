4c091d682fffab581ac2003ddcc4cd839cb7fdb7
HassanBahati <mukisabahati@gmail.com>
2021-07-13 23:49:03 +0300

    added computation to factorial

--- src/components/Factorial/Factorial.jsx
+++ src/components/Factorial/Factorial.jsx
@@ -1,34 +1,73 @@
 import React, { useEffect, useState } from "react";
 
 function Factorial() {
-  // Declare a new state variable, which we'll call "count"
-  const [challenge, setChallenge] = useState(0);
-  const [result, setResult] = useState(challenge);
+  const [challenge, setChallenge] = useState("");
+  const [result, setResult] = useState("");
 
-  function factorial(challenge) {
-    //base case
-    if (challenge === 0 || challenge === 1) {
-      return 1;
-      //recursive case
-    } else {
-      return challenge * factorial(challenge - 1);
+  useEffect(() => {
+    const factorial = (challenge) => {
+      if (challenge === 0 || challenge === 1) {
+        return 1;
+      } else {
+        let factorialResult = 1;
+
+        for (var i = challenge; i >= 1; i--) {
+          factorialResult = factorialResult * i;
+        }
+        return factorialResult;
+      }
+    };
+    setResult(factorial(challenge));
+  }, [challenge]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    try {
+      console.log({
+        challenge: challenge,
+        result: result,
+      });
+      // axios.post('https://codechallengeserver.herokuapp.com/api/v1/numbers', data)
+      //     .then(response => {
+      //         console.log(response)
+      //     })
+      //     .catch(error => {
+      //         console.log(error);
+      //     })
+    } catch (error) {
+      console.log(error);
     }
+    setChallenge("");
   }
-
-  useEffect(() => {}, []);
-
-  const handleClick = () => {
-    setResult(factorial);
-  };
-
   return (
     <div>
-      <button onClick={handleClick}>Click me</button>
-      <p>
-        {challenge}! = {result === challenge ? 'enter a number then hit the calculate button' : result }
-      </p>
+      <div>
+        <h1>Factorial</h1>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input
+          name="challenge"
+          type="number"
+          placeholder="Enter number here"
+          value={challenge}
+          onChange={(e) => setChallenge(e.target.value)}
+        />
+        {!challenge ? "" : <button type="submit">Clear</button>}
+      </form>
+      <div>
+        {!challenge ? (
+          ""
+        ) : (
+          <div>
+            Computation = {challenge}! <br />
+            Result = {result}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
 
 export default Factorial;
+
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Factorial from '../components/Factorial/Factorial'
+import SquareRoot from '../components/SquareRoot/SquareRoot'
 
 function Home() {
     return (
         <div>
             we are home 
             <Factorial/>
+            trial
+            <SquareRoot/>
+            
         </div>
     )
 }
